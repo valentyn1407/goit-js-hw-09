a5933de9a7d707bbeb95da9e3db633de71a83406
valentyn1407 <130169250+valentyn1407@users.noreply.github.com>
2023-09-16 14:23:15 +0000

    Deploying to gh-pages from @ valentyn1407/goit-js-hw-09@de407700b9b027d70fa9a183e08df580d754fb53 🚀

--- 03-promises.2d502cdb.js
+++
@@ -1,2 +0,0 @@
-var o,e;(o=2,e=1500,new Promise(((i,s)=>{setTimeout((()=>{Math.random()>.3?i({position:o,delay:e}):s({position:o,delay:e})}),e)}))).then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.2d502cdb.js.map
